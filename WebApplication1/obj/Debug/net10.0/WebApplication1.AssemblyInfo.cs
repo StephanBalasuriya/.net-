9d36f2262e0f13ffb127efab8fb7bfde9bc749f6
Stephan Balasuriya <stephan.effectivesolutions.com>
2025-12-31 11:29:26 +0530

    reduce logging level by       "Microsoft.EntityFrameworkCore.Database.Command": "Warning"  adding to app settings

--- WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac4e4dba61d14f0f9bec136e712920fc76af5a9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6126486f06bc8e93e15d0b0fc53cbb7349ac44fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
